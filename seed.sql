85f8a0d71d0f096ea9cb3d28ed560e5f7219e892
Van Tran <vantran@pursuit.org>
2019-03-24 13:35:35 -0400

    updated seed

--- seed.sql
+++ seed.sql
@@ -1,41 +1,32 @@
 DROP DATABASE IF EXISTS patchthat;
 CREATE DATABASE patchthat;
 
-\c shopped;
+\c patchthat;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   username VARCHAR (100) UNIQUE NOT NULL,
-  name VARCHAR(100) NOT NULL,
   email VARCHAR (100) UNIQUE NOT NULL,
-  creditCard INT NULL,
-  address VARCHAR NULL,
-  country VARCHAR NULL,
-  state VARCHAR NULL,
-  zipCode VARCHAR NULL,
-  shopName VARCHAR (100) NULL,
-  shopDescrip VARCHAR NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
-  token VARCHAR NULL
+  token VARCHAR NULL,
 );
 
--- CREATE TABLE shop (
---   shop_id SERIAL PRIMARY KEY,
---   seller INT REFERENCES users(id) NOT NULL,
---   shop_name VARCHAR (100) NOT NULL,
---   description VARCHAR
--- );
+CREATE TABLE shop (
+shop_id SERIAL PRIMARY KEY,
+seller INT REFERENCES users(id) NOT NULL,
+shop_name VARCHAR (100) NOT NULL,
+description VARCHAR
+);
 
-CREATE TABLE products (
-  prod_id SERIAL PRIMARY KEY,
-  userid INT REFERENCES users(id) NOT NULL,
-  name VARCHAR (100) NOT NULL,
+CREATE TABLE patches (
+  patch_id SERIAL PRIMARY KEY,
+  shop_id INT REFERENCES shops(id) NOT NULL,
+  patch_name VARCHAR (100) NOT NULL,
   description VARCHAR,
   price FLOAT(2) NOT NULL,
   category VARCHAR (100) NOT NULL,
   ratings INT NULL,
-  size INT NULL
+  size INT NULL,
+  image_url INT NULL,
 );
 
 CREATE TABLE cart (
@@ -63,11 +54,21 @@ CREATE TABLE orderItems (
   orderItemsid SERIAL PRIMARY KEY,  
   orderid INT REFERENCES orders(orderid) NOT NULL,
   prod_id INT REFERENCES products(prod_id) NOT NULL,
-  quantity INT NOT NULL
+  quantity INT NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  payment_token INT NULL,
+  address VARCHAR NULL,
+  country VARCHAR NULL,
+  currentState VARCHAR NULL,
+  zipCode VARCHAR NULL,
+  shopName VARCHAR (100) NULL,
+  shopDescrip VARCHAR NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
 );
 
-CREATE TABLE comments (
-  comment_id SERIAL PRIMARY KEY,
+CREATE TABLE rating (
+  rating_id SERIAL PRIMARY KEY,
   prod_id INT REFERENCES products(prod_id) NOT NULL,
   users_id INT REFERENCES users(id) NOT NULL,
   comment VARCHAR NOT NULL,
@@ -77,11 +78,12 @@ CREATE TABLE comments (
 );
 
 
+INSERT INTO users (username, email, token) VALUES
+('MarilynPatches', 'marilynpatches@gmail.com', '1234');
+('mo.mo', 'mo.mo@gmail.com', '12345');
+('patch.dude', 'patch.dude@gmail.com', '123456');
 
-
-
--- INSERT INTO users (username, name, email) VALUES
--- ('J123', 'John', 'john@email.com');
-
--- INSERT INTO posts (author, title, body) VALUES
--- (1, 'bunny', 'Marshmello'), (2, 'dog', 'Chai'), (1, 'cat', 'Pixel');
+INSERT INTO shop (seller, shop_name, description) VALUES
+('MarilynPatches', 'MarilynMakesPatches', '');
+('mo.mo', 'MoPatchShop', '12345');
+('patch.dude', 'Patch.Dudes', '123456');
